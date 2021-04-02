11a56169e4127e79760832923c7ccfb9b8b173b1
allysierra <77810933+allysierra@users.noreply.github.com>
2021-04-01 20:19:28 -0400

    Add files via upload

---
+++ hw7-2021-main/js/video.js
@@ -0,0 +1,69 @@
+var video = document.querySelector("#player1")
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML=video.volume *100 + "%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up Video");
+	video.playbackRate /= 0.95;
+	console.log("New speed is "+ video.playbackRate);
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down Video");
+	video.playbackRate *= 0.95;
+	console.log("New speed is "+ video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if(video.currentTime < video.duration - 15){
+		video.currentTime += 15;
+	}
+	else{
+		video.currentTime = 0;
+		video.play();
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if(video.muted){
+		video.muted = false;
+		this.innerHTML="Unmute";
+	}
+	else{
+		video.muted = true;
+		this.innerHTML="Mute";
+	}
+	console.log("Mute Video");
+
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	console.log("Changing volume");
+	console.log(this.value);
+	video.volume = this.value /100;
+	document.querySelector("#volume").innerHTML=video.volume *100 + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("Changing CSS");
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("Removing CSS");
+	video.classList.remove("oldSchool");
+});
